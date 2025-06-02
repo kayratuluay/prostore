445277e45e7b9ab1a876c3bb5681aeeff9e41152
Kayra Tuluay <kayratuluay@gmail.com>
2025-06-02 17:23:17 +0300

    main 10 | Product view components has been created.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,17 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 const Homepage = async () => {
-  return <>ProStore</>;
+  return (
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList
+        data={sampleData.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </div>
+  );
 };
 
 export default Homepage;
